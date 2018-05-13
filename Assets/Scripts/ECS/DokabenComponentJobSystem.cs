51e8b29a92cacc809088f7b3481ac83157355bdc
mao <miymao78.d@gmail.com>
2018-05-14 08:30:10 +0900

    C#JobSystemも併用して動かすように設定 ※PlayerSettingsから"ENABLE_JOBSYSTEM"のシンボルを消すことでPureECSのみの動作となる

--- Assets/Scripts/ECS/DokabenComponentJobSystem.cs
+++ Assets/Scripts/ECS/DokabenComponentJobSystem.cs
@@ -4,14 +4,15 @@ using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
 using Unity.Collections;
+using Unity.Jobs;
 
-#if !ENABLE_JOBSYSTEM
+#if ENABLE_JOBSYSTEM
 namespace MainContents.ECS
 {
     /// <summary>
-    /// ドカベンロゴの回転(ComponentSystem)
+    /// ドカベンロゴの回転(ComponentSystem) ※JobSystem併用版
     /// </summary>
-    public class DokabenComponentSystem : ComponentSystem
+    public class DokabenComponentJobSystem : ComponentSystem
     {
         // Systemで要求されるComponentData
         struct Group
@@ -21,27 +22,26 @@ namespace MainContents.ECS
             public ComponentDataArray<TransformMatrix> Transforms;
         }
 
-        [Inject] Group _group;
-
-        protected override void OnUpdate()
+        struct DokabenParallelForUpdate : IJobParallelFor
         {
-            float time = Time.time;
-            int animLength = Constants.AnimationTable.Length;
+            public Group Group;
+            [ReadOnly] public float Time;
 
-            for (int i = 0; i < this._group.Length; i++)
+            // Jobで実行されるコード実行
+            public void Execute(int index)
             {
-                var data = this._group.Dokabens[i];
+                var data = this.Group.Dokabens[index];
                 float4x4 m = float4x4.identity;
 
                 // 時間の正弦を算出(再生位置を加算することで角度をずらせるように設定)
-                float sinTime = math.sin(time * Constants.AnimationSpeed) + data.AnimationHeader;
+                float sinTime = math.sin(this.Time * Constants.AnimationSpeed) + data.AnimationHeader;
 
                 // _SinTime0~1に正規化→0~15(コマ数分)の範囲にスケールして要素数として扱う
                 float normal = (sinTime + 1f) / 2f;
 
                 // X軸に0~90度回転
-                var index = (int)math.round(normal * (animLength - 1));
-                float rot = Constants.AnimationTable[index] * math.radians(90f);
+                var animIndex = (int)math.round(normal * (Constants.AnimationTable.Length - 1));
+                float rot = Constants.AnimationTable[animIndex] * math.radians(90f);
 
                 // 任意の原点周りにX軸回転を行う(原点を-0.5ずらして下端に設定)
                 // ※Matrix4x4は列優先でfloat4x4は行優先みたいなので注意
@@ -63,12 +63,28 @@ namespace MainContents.ECS
 
 
                 // 計算結果の保持
-                this._group.Dokabens[i] = data;
+                this.Group.Dokabens[index] = data;
 
-                var trs = this._group.Transforms[i];
+                var trs = this.Group.Transforms[index];
                 trs.Value = m;
-                this._group.Transforms[i] = trs;
+                this.Group.Transforms[index] = trs;
             }
+        }
+
+        [Inject] Group _group;
+
+        protected override void OnUpdate()
+        {
+            // 回転計算用jobの作成
+            DokabenParallelForUpdate rotateJob = new DokabenParallelForUpdate()
+            {
+                Time = Time.time,
+                Group = this._group,
+            };
+            // Jobの実行
+            var jobHandle = rotateJob.Schedule(this._group.Length, 7);
+            // Jobが終わるまで処理をブロック
+            jobHandle.Complete();
         }
     }
 }
